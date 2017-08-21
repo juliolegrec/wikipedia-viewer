a1f1d377bdb6f2b3c10e776e4ca5f17534113224
Julio Legrec <julio.legrec@gmail.com>
2017-08-21 18:18:10 +0400

    Completed from end design and coding of the app

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
-// Give this project some life!!!
+let searchBtn = document.querySelector('.btn-search'),
+    randomBtn = document.querySelector('.btn-random');
+    
+searchBtn.addEventListener('click', () => {
+  let searchValue = document.querySelector('.search-input').value,
+      xhr = new XMLHttpRequest(),
+      method = "GET",
+      url = `https://en.wikipedia.org/w/api.php?action=query&titles=${searchValue}&origin=*&prop=revisions&rvprop=content&format=json`;
+
+  xhr.open(method, url, true);
+  // xhr.setRequestHeader("Origin", "http://localhost:3000/");
+  xhr.onreadystatechange = function () {
+    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+
+      let response = JSON.parse(xhr.responseText);
+      console.log(response.query.pages);
+
+    }
+  };
+  xhr.send();
+})
