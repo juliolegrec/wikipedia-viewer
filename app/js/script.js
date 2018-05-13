affab3da149f229988a4e29dd094d186af7bbc37
Julio <TheGreek@Julios-iMac.local>
2018-05-13 12:31:10 +0400

    build app more specifically

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,8 @@
 let searchBtn = document.querySelector('.btn-search'),
     randomBtn = document.querySelector('.btn-random');
     
-searchBtn.addEventListener('click', () => {
+searchBtn.addEventListener('click', (e) => {
+	e.preventDefault();
   let searchValue = document.querySelector('.search-input').value,
       xhr = new XMLHttpRequest(),
       method = "GET",
@@ -11,15 +12,26 @@ searchBtn.addEventListener('click', () => {
 
   xhr.open(method, url, true);
   // xhr.setRequestHeader("Origin", "http://localhost:3000/");
+	let results = document.querySelector('.results');
   xhr.onreadystatechange = function () {
     if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
 
       let response = JSON.parse(xhr.responseText);
-      let pages = Object.values(response.query.pages);
+			let pages = Object.values(response.query.pages);
+
+			console.table(pages);
+			results.removeChild('a');
+
       for(let i = 0; i < pages.length; i++) {
-        console.log(pages[i].title);
-        console.log(pages[i].extract);
-      }
+				let result = document.createElement('a');
+				// result.innerHTML = `<h2>${pages[i].title}</h2><p>${pages[i].extract}</p>`;
+				result.setAttribute('class', 'result');
+				result.setAttribute('href', `https://en.wikipedia.org/?curid=${pages[i].pageid}`);
+				result.setAttribute('target', '_blank');
+				result.innerHTML = `<h2>${pages[i].title}</h2><p>${pages[i].extract}</p>`;
+				results.appendChild(result);
+			}
+			
     }
   };
   xhr.send();
